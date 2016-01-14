97e4acda4d83b294a313a76a64664db2b62f065f
James Monger <jameskmonger@hotmail.co.uk>
2016-01-14 19:04:43 +0000

    gulpfile.ts rather than .js

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,29 +1,32 @@
-(function() {
-  "use strict";
-
-  const gulp = require('gulp');
-  const tsc = require('gulp-typescript');
-  const Server = require('karma').Server;
-
-  const srcTSConfig = tsc.createProject('src/tsconfig.json');
-  const specTSConfig = tsc.createProject('spec/tsconfig.json');
-
-  gulp.task('test', ['test-build:spec', 'test-build:src'], () => {
-    new Server({
-      configFile: __dirname + '/build.spec/spec/karma.conf.js',
-      singleRun: true
-    }).start();
-  });
-  gulp.task('test-build:spec', (done) => {
-    specTSConfig.src()
-      .pipe(tsc(specTSConfig))
-      .pipe(gulp.dest('build.spec'))
-      .on('end', done);
-  });
-  gulp.task('test-build:src', (done) => {
-    srcTSConfig.src()
-      .pipe(tsc(srcTSConfig))
-      .pipe(gulp.dest('build.spec/src'))
-      .on('end', done);
-  });
+/// <reference path="typings/requirejs/require.d.ts" />
+var GulpEnvironment = (function () {
+    function GulpEnvironment() {
+        var gulp = require('gulp');
+        var tsc = require('gulp-typescript');
+        var Server = require('karma').Server;
+        var srcTSConfig = tsc.createProject('src/tsconfig.json');
+        var specTSConfig = tsc.createProject('spec/tsconfig.json');
+        gulp.task('test', ['test-build:spec', 'test-build:src'], function () {
+            new Server({
+                configFile: __dirname + '/build.spec/spec/karma.conf.js',
+                singleRun: true
+            }).start();
+        });
+        gulp.task('test-build:spec', function (done) {
+            specTSConfig.src()
+                .pipe(tsc(specTSConfig))
+                .pipe(gulp.dest('build.spec'))
+                .on('end', done);
+        });
+        gulp.task('test-build:src', function (done) {
+            srcTSConfig.src()
+                .pipe(tsc(srcTSConfig))
+                .pipe(gulp.dest('build.spec/src'))
+                .on('end', done);
+        });
+    }
+    return GulpEnvironment;
+})();
+(function () {
+    new GulpEnvironment();
 })();
