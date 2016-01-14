4e7032551cab2d5110a20cd5c395624c3e6635ae
James Monger <jameskmonger@hotmail.co.uk>
2016-01-14 18:09:13 +0000

    Fixed test-main.ts

--- spec/test-main.ts
+++ spec/test-main.ts
@@ -1,29 +1,26 @@
 /// <reference path="../typings/requirejs/require.d.ts" />
 
 class TestMain {
-  private files: Array<any>;
-  private MATCHER: RegExp;
-
   constructor() {
-    this.files = new Array<any>();
-    this.MATCHER = /.*(\.spec\.js)/i;
+    const files: Array<any> = new Array<any>();
+    const MATCHER: RegExp = /.*(\.spec\.js)/i;
 
     Object.keys((window as any).__karma__.files).forEach(function(file) {
-      if (this.MATCHER.test(file)) {
+      if (MATCHER.test(file)) {
         // Normalize paths to RequireJS module names.
         // If you require sub-dependencies of test files to be loaded as-is (requiring file extension)
         // then do not normalize the paths
         var normalizedTestModule = file.replace(/^\/base\/|\.js$/g, '');
-        this.files.push(normalizedTestModule);
+        files.push(normalizedTestModule);
       }
-    }.bind(this));
+    });
 
     require.config({
       // Karma serves files under /base, which is the basePath from your config file
       baseUrl: '/base',
 
       // dynamically load all test files
-      deps: this.files,
+      deps: files,
 
       // we have to kickoff jasmine, as it is asynchronous
       callback: (window as any).__karma__.start
