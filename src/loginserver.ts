cb7fc3e7e958f90c7d3aec4af72fa08b9b73996c
jamesrichford <jamesrichford@googlemail.com>
2016-01-21 19:35:17 +0000

    Add basic location tracking ability

--- src/loginserver.ts
+++ src/loginserver.ts
@@ -19,6 +19,10 @@ app.get('/', function(req, res){
   res.sendFile(path.resolve(__dirname, '../index.html'));
 });
 
+app.post('/session', (request, response) {
+   response.send({ serverLocation: "http://localhost:4001"})
+});
+
 app.listen(3000, () => {
   console.log('app server listening on 3000');
 });
@@ -30,9 +34,51 @@ var httpSocket = (http as any).Server(appSocket);
 var ioSocket = io(httpSocket);
 
 ioSocket.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    ioSocket.emit('chat message', msg);
+   var findClientsSocket = function(roomId, namespace) {
+    var res = []
+    , ns = ioSocket.of(namespace ||"/");    // the default namespace is "/"
+
+    if (ns) {
+        for (var id in ns.connected) {
+            if(roomId) {
+                var index = ns.connected[id].rooms.indexOf(roomId) ;
+                if(index !== -1) {
+                    res.push(ns.connected[id]);
+                }
+            } else {
+                res.push(ns.connected[id]);
+            }
+        }
+    }
+    return res;
+}
+
+   var sockets = findClientsSocket();
+   for(var i = 0; i < sockets.length; i++) {
+      if (sockets[i].position) {
+      console.log("sending everyone online", { id: sockets[i].id, position: sockets[i].position });
+      socket.emit("update-position", { id: sockets[i].id, position: sockets[i].position });
+   }
+   }
+
+  //when a player sends a position update
+  socket.on('update-position', (position) => {
+     console.log(position);
+     socket.position = position;
+     //broadcast that position to everyone else
+    ioSocket.emit('update-position', {
+         id: socket.id,
+         position: position
+      });
   });
+
+  //notify all users of a newly connected user
+  ioSocket.emit('chat message', "connected");
+
+  //notify the user when a user disconnects
+  socket.on("disconnect", () => {
+     ioSocket.emit("player-disconnect", { id: socket.id });
+ });
 });
 
 httpSocket.listen(4001, () => {
